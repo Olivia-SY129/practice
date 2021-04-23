e59fdf62a3edb2ef710029c59eaa30ece0578bbe
Olivia-SY129 <81205807+Olivia-SY129@users.noreply.github.com>
2021-04-23 15:20:24 +0900

    Update index.js

--- DdayToXmas/src/index.js
+++ DdayToXmas/src/index.js
@@ -4,7 +4,7 @@ const time = {}
 // You're gonna need this
 function getTime() {
   // Don't delete this.
-    const xmasDay = new Date("2021-12-24:00:00:00+0900");
+    const xmasDay = new Date("2021/12/24:00:00:00");
     const today = new Date();
 
     //   total value
